e239e695f5e01ba21fee61518bee38b00dcf8daa
dat-ayush <ayushkr91997@gmail.com>
2024-09-12 00:53:56 +0530

    Create consts.py

---
+++ fkart/consts.py
@@ -0,0 +1,29 @@
+# HTTP Request headers & data
+# make sure to check and replace with new parameters
+# to reduce future errors
+
+request_headers = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Upgrade-Insecure-Requests": "1",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "none",
+    "Sec-Fetch-User": "?1",
+    "Priority": "u=0, i",
+    "Te": "trailers",
+    "Connection": "keep-alive"
+}
+
+
+request_data = b'\x0d\x0a\x0d\x0a'
+
+request_cookies = {"solved_captcha": "1726074885-17217-10945480-9f77016cbd52a35fa19438f2aeea0a4115fe1a07e0a4d586c4f97a15b743d562"}
+
+price_identifier = {
+    "tag": "div",
+    "class": "Nx9bqj CxhGGd"
+}
+
+default_userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.6533.100 Safari/537.36"
